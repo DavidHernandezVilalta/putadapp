8d5a969496d5b13d86dc9c1cb23697b670f44e49
David Hernandez Vilalta <david.hernandez.vilalta@est.fib.upc.edu>
2017-07-04 18:14:51 +0200

    21.57 3/7/17

--- app/src/main/java/com/davidhernandezvilaltagmail/projecte1/Login.java
+++ app/src/main/java/com/davidhernandezvilaltagmail/projecte1/Login.java
@@ -14,22 +14,18 @@ import android.widget.Toast;
 
 import com.davidhernandezvilaltagmail.projecte1.R;
 import com.davidhernandezvilaltagmail.projecte1.BaseActivity;
+import com.davidhernandezvilaltagmail.projecte1.activities.Activity2;
 import com.davidhernandezvilaltagmail.projecte1.activities.Calculator;
 import com.davidhernandezvilaltagmail.projecte1.activities.ForgottenPassword;
 import com.davidhernandezvilaltagmail.projecte1.activities.Loginfail;
+import com.davidhernandezvilaltagmail.projecte1.activities.Signup;
 import com.davidhernandezvilaltagmail.projecte1.database.MyDataBaseHelper;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class Login extends AppCompatActivity{
     TextView tv;
     Button login, signup;
-    Set<String> users = new HashSet<String>();
     EditText username, password;
-    Map<String, String> hm = new HashMap<String, String>();
     private MyDataBaseHelper myDataBaseHelper;
 
     @Override
@@ -59,14 +55,16 @@ public class Login extends AppCompatActivity{
             signup.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
-                    if (username == null||password == null){
+                    /*if (username == null||password == null){
                         Toast.makeText(getApplicationContext(), "Falta algun camp per emplenar", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     String s = username.getText().toString();
                     String s1 = password.getText().toString();
                     long id = myDataBaseHelper.createRow(s, s1);
-                    Toast.makeText(v.getContext(),"Has singejat in!!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(v.getContext(),"Has singejat in!!", Toast.LENGTH_SHORT).show();*/
+                    Intent i = new Intent(getApplicationContext(), Signup.class);
+                    startActivity(i);
                 }
             });
             login.setOnClickListener(new View.OnClickListener(){
@@ -85,7 +83,8 @@ public class Login extends AppCompatActivity{
                         SharedPreferences.Editor editor = settings.edit();
                         editor.putBoolean("logged", true);
                         editor.apply();
-                        Intent i = new Intent(getApplicationContext(), Calculator.class);
+                        Intent i = new Intent(getApplicationContext(), Activity2.class);
+                        finish();
                         startActivity(i);
                     }
                     else if (myDataBaseHelper.queryUser(s).equals("0")) Toast.makeText(v.getContext(), "Incorrect password", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/davidhernandezvilaltagmail/projecte1/activities/Mediaplayer.java
+++ app/src/main/java/com/davidhernandezvilaltagmail/projecte1/activities/Mediaplayer.java
@@ -5,17 +5,11 @@ import android.os.Bundle;
 
 import com.davidhernandezvilaltagmail.projecte1.R;
 
-public class ForgottenPassword extends AppCompatActivity {
+public class Mediaplayer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.forgotten_password_activity);
-    }
-
-    @Override
-    public void onBackPressed() {
-        finish();
-        super.onBackPressed();
+        setContentView(R.layout.activity_mediaplayer);
     }
 }
